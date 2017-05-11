7339b7de50f945d22550e81fa15a6381039fdd14
gagansuntle <gagan@suntle.com>
2017-05-11 14:35:15 +0530

    crispy forms added

--- redux/settings.py
+++ redux/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'register'
+    'register',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -115,6 +116,8 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
+CRISPY_TEMPLATE_PACK = 'bootstrap3'
 
 
 ACCOUNT_ACTIVATION_DAYS = 7
