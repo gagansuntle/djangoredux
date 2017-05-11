9d17761b58dca46bb1a21cf9c114ec25806adb05
gagansuntle <gagan@suntle.com>
2017-05-11 15:14:10 +0530

    added bootstrap static files

--- redux/settings.py
+++ redux/settings.py
@@ -126,6 +126,7 @@ REGISTRATION_AUTO_LOGIN = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 SITE_ID = 1
